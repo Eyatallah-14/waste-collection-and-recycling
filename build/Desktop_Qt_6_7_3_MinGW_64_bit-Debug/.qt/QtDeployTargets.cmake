462d04736ab21c4fe6ddebfbfaa4450fb0fc4811
dhia <DhiaEddine.BOULARES@esprit.tn>
2026-02-16 19:23:30 +0100

    Remove build files from repo

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_emp_vehic_zones_poubelle_FILE C:/Users/21621/Documents/sans_titre4/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/emp_vehic_zones_poubelle.exe)
-set(__QT_DEPLOY_TARGET_emp_vehic_zones_poubelle_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_emp_vehic_zones_poubelle_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
